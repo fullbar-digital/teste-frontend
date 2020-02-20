601384be6804536c82d1f74f32dc2b717114e291
Mariana Oliveira <olvr.mariana@gmail.com>
2020-02-20 00:26:54 -0300

    Install axios and consume the Pokémon API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import './App.css';
+import axios from 'axios';
+// import PokemonList from './components/PokemonList';
 
-function App() {
+const api = {
+  baseUrl: "https://pokeapi.co/api/v2/pokemon/"
+};
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      pokemonData: []
+    };
+}
+
+componentDidMount() {
+axios.get(api.baseUrl) //fetch request
+    .then((res) => {
+      console.log(res);
+      this.setState({ pokemonData: res.data.results });
+    });
+}
+
+  render() {
+    const { pokemonData } = this.state;
   return (
     <div className="App">
+      {pokemonData.map((results) => (
+      <div>
+
+      <p>{results.name}</p>
+      <p>{results.url}</p>
       
+      </div>
+    )
+    )}
     </div>
-  );
-}
+  )
+}}
 
 export default App;
