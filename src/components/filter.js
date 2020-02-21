33a55c9f15a643163b83bd7707d868e74a5da791
Mariana Oliveira <olvr.mariana@gmail.com>
2020-02-21 20:49:27 -0300

    Create a filter component and a little stylization

--- src/components/filter.js
+++ src/components/filter.js
@@ -4,7 +4,7 @@ export default class filter extends Component {
     render() {
         return (
             <div>
-                <input type="search" class="" placeholder="Número do pokémon inicial que será carregado"></input>
+                <input type="search" class="" placeholder=""></input>
             </div>
         )
     }
