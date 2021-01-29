8ba9cca97570e6f71779a3191c0d196d07a58f0c
Daniel André <daniandreds@gmail.com>
2021-01-29 10:05:47 -0300

    commit inicial

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header = () => {
+  return <header className={styles.header}>Header</header>;
+};
+
+export default Header;
