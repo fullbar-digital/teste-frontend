115d5aae4ae417ad3303e5fb3bc5fe0e9bc9769f
[Rafael Assad] <[rafael.assad1010@gmail.com]>
2021-02-02 12:42:08 -0300

    chore: instaled dependencies and initial structure created

---
+++ poklist/src/routes.jsx
@@ -0,0 +1,11 @@
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route/>
+        </Switch>
+    )
+}
+
+export default Routes
