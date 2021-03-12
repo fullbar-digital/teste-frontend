b32c49b0d2538d02a923619335ac657bb7278286
Adriano Franco <adrianofranco@adrianofranco.com.br>
2021-03-12 07:39:19 -0300

    Qual pokemon inicio

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,39 +4,57 @@ import '../components/Pokedex.scss'
 
 import axios from 'axios';
 
-import ReactPaginate from 'react-paginate';
+//import ReactPaginate from 'react-paginate';
 
 import PokedexItens from '../components/pokemonItens/PokemonsItens';
+import FilterItens from '../components/FilterItens/FilterItens';
 
 export default class Pokedex extends Component {
 
 
     state = { 
-        url: 'https://pokeapi.co/api/v2/pokemon/limit=898',
-        pokemons: []
+        url: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9',
+        urlPokemonItem: 'https://pokeapi.co/api/v2/pokemon-form/',
+        pokemons: [],
+        pokemon: null
+
     }
 
+  
     componentDidMount() {
         this.getPokemons();
+        
     }
 
+    handleCallback = (childData) =>{
 
+        
+            axios.get(this.state.urlPokemonItem + childData)
+              .then(res => {
+                const pokemon = res.data;
+                this.setState({ pokemon });
+              })
+        
+
+    }
     
     getPokemons() {
         axios.get(this.state.url)
           .then(res => {
             const pokemons = res.data['results'];
             this.setState({ pokemons });
-            
           })
     }
 
     render() {
 
+        const {pokemon} = this.state;
+
         return (
             <div className="main">
+                <FilterItens pokemon={pokemon} ></FilterItens>
                 <div className="pokemons">
-                    <PokedexItens pokemonsItens={this.state.pokemons}></PokedexItens>
+                    <PokedexItens pokemonsItens={this.state.pokemons} parentCallback={this.handleCallback}></PokedexItens>
                 </div>
             </div>
         )
--- src/components/pokemonItens/PokemonsItens.js
+++ src/components/pokemonItens/PokemonsItens.js
@@ -2,50 +2,40 @@ import React, { Component } from 'react'
 
 import './PokemonsItens.scss'
 
-//import { LazyLoadImage } from 'react-lazy-load-image-component';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 export default class PokemonsItens extends Component {
 
-
-
-    pad_with_zeroes(number, length) {
-
-        var my_string = '' + number;
-        while (my_string.length < length) {
-            my_string = '0' + my_string;
-        }
-    
-        return my_string;
-    
+    onTrigger = (event) => {
+        this.props.parentCallback(event.target.id);
+        event.preventDefault();
     }
-
-
-
-    getImage(index){
-
-        
-        index++;
-
-        index = this.pad_with_zeroes(index, 3);
-
-        return 'https://play-lh.googleusercontent.com/EDYskRd9dnqmKfQP5KxjJHj5NgbWDiS9zceSmfYRgeDaQRQnaiRyZIQNeoX5AIIZ5VY=s180-rw';
-
-        //return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${index}.png`;
-
-        //return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`
-
-    }
-
-
 
     render() {
-        return (
 
-             this.props['pokemonsItens'].map((pokemon, i) => 
-            <div key={i} className="pokemon-item" >
-                <div className="pokemon-img"><img src={this.getImage(i)} alt={pokemon.name} /></div>
-                 <div className="pokemon-name center-hozintal">{pokemon.name}</div>
-                 </div>)
+        return (
+            this.props['pokemonsItens'].map((pokemon, i) =>
+                <div key={i} onClick={this.onTrigger} className="pokemon-item" >
+                    <div className="pokemon-img">
+                        <LazyLoadImage
+                            id={pokemon.name}
+                            alt={pokemon.name}
+                            height={96}
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i + 1}.png`} // use normal <img> attributes as props
+                            effect="blur"
+                            width={96} />
+
+                        <LazyLoadImage
+                            id={pokemon.name}
+                            alt={pokemon.name}
+                            height={96}
+                            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${i + 1}.png`} // use normal <img> attributes as props
+                            effect="blur"
+                            width={96} />
+
+                    </div>
+                    <div className="pokemon-name center-hozintal">{pokemon.name}</div>
+                </div>)
 
         )
     }
