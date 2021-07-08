86f41f2484f257b84fcdff6b0b369c2a6a8e8437
Eric Martins <ericmartins1997@gmail.com>
2021-07-08 13:15:57 -0300

    feature: create header and nav

---
+++ pokestore/src/components/header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+    <Nav />
+    <div class="header">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from "../nav/Nav.vue";
+
+
+export default {
+  components: {
+    Nav,
+  },
+  data: () => ({
+  }),
+
+  methods: {},
+};
+</script>
+
+<style src="./Style.scss" lang="scss" scoped/>
---
+++ pokestore/src/components/nav/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav">
+    <div class="nav-content">
+      <img class="background-nav" src="../../assets/Pokemon-Logo.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+  },
+};
+</script>
+
+
+<style src="./Style.scss" lang="scss" scoped/>
