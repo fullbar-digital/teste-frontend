cbaf4bc63c788b46e0316dda8eb1ba7e69346dab
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-21 12:45:03 -0300

    feat: Cria-se o componente Footer
    Neste componente apenas mostra o nome do desenvolvedor.

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./style.scss";
+
+export const Footer = () => {
+  return (
+    <>
+      <footer id="footer">
+        <div className="footer__area container">
+          Desenvolvido por gean_ferreira &#9830;
+        </div>
+      </footer>
+    </>
+  );
+};
