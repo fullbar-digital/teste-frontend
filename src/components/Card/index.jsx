627c1d1f06626013e38eb17f2771fd732452b238
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-21 15:51:52 -0300

    feat: Adiciona nova prop (id) no componente Card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,12 @@
 import "./style.scss";
 
-export const Card = ({ url, name, img }) => {
+export const Card = ({ url, id, name, img }) => {
   return (
     <>
       <div className="card">
         <img src={img} alt={`Imagem do pokemon ${name}`} />
         <div className="card__information">
-          <span className="card__information__id">nº id</span>
+          <span className="card__information__id">nº {id}</span>
           <span className="card__information__name">{name}</span>
           <ul className="card__information__abilities">
             <li>fire</li>
