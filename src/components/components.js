5af9f348569efb7dd38bbed3ea6ca7eb12150c45
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-21 20:55:34 -0300

    Coloca o componente Modal no arquivo js que exporta todos os componentes em um só local

--- src/components/components.js
+++ src/components/components.js
@@ -3,5 +3,6 @@ import { Pokedex } from "./PokemonList/index";
 import { Loading } from "./Loading/index";
 import { Header } from "./Header/index";
 import { Card } from "./Card/index";
+import { Modal } from "./Modal/index";
 
-export { Footer, Pokedex, Loading, Card, Header };
+export { Footer, Pokedex, Loading, Card, Header, Modal };
