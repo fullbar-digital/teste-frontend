e7bd0f441addaad0f3bdac8511e7b697582542a2
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-21 12:55:25 -0300

    file: Arquivo que armazenará todos os components para exportação
    Motivo deste commit é reduzir a quantidade de import nos componentes, visto que, alguns componentes importará mais que um componente, assim, deixando a aplicação enorme.

---
+++ src/components/components.js
@@ -0,0 +1,7 @@
+import { Footer } from "./Footer/index";
+import { Pokedex } from "./PokemonList/index";
+import { Loading } from "./Loading/index";
+import { Header } from "./Header/index";
+import { Card } from "./Card/index";
+
+export { Footer, Pokedex, Loading, Card, Header };
