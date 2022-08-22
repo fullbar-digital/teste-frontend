33e6f6e2a06a4c4089fd236a8478be8ef214297b
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-22 15:57:21 -0300

    Anexando o componente ao arquivo default de exportação de todos os componentes

--- src/components/components.js
+++ src/components/components.js
@@ -5,5 +5,6 @@ import { Header } from "./Header/index";
 import { Card } from "./Card/index";
 import { Modal } from "./Modal/index";
 import { Info } from "./Info/index";
+import { Filter } from "./Filter/index";
 
-export { Footer, Pokedex, Loading, Card, Header, Modal, Info };
+export { Footer, Pokedex, Loading, Card, Header, Modal, Info, Filter };
