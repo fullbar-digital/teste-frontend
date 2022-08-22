403df4299b82094d457d620ef23b4fc03ed81cfc
Gean Ferreira <gean_roberto@hotmail.com>
2022-08-21 23:08:44 -0300

    feat: Cria o componente Info
    Este componente sera o responsavel pelo conteúdo detalhado dos pokemons. Este componente será renderizado dentro da Modal.

--- src/components/components.js
+++ src/components/components.js
@@ -4,5 +4,6 @@ import { Loading } from "./Loading/index";
 import { Header } from "./Header/index";
 import { Card } from "./Card/index";
 import { Modal } from "./Modal/index";
+import { Info } from "./Info/index";
 
-export { Footer, Pokedex, Loading, Card, Header, Modal };
+export { Footer, Pokedex, Loading, Card, Header, Modal, Info };
