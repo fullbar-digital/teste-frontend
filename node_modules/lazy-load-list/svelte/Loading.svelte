9db1928a42af63e24b428ccea9f043b4baa3d5fe
Gabriel <gabrielfcdossantos@gmail.com>
2022-08-22 11:58:07 -0400

    Teste frontend

---
+++ node_modules/lazy-load-list/svelte/Loading.svelte
@@ -0,0 +1,10 @@
+<script>
+    export const color = '#18191A';
+    import '../lib/index.css'
+</script>
+
+<div class="dots">
+    <div style={`background-color: ${color};`}></div>
+    <div style={`background-color: ${color};`}></div>
+    <div style={`background-color: ${color};`}></div>
+</div>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <img class="image" src="../assets/Pokemon-Logo.png" />
+  </div>
+  <div><Filter /></div>
+  <div class="home">
+     <Card
+      :number="index"
+      :itens="pokemon"
+      v-for="(pokemon, index) in this.$store.state.allPoke"
+      :key="index"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card/Card.vue';
+import Filter from '@/components/Filter/Filter.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    Card,
+    Filter,
+  },
+  data: () => ({
+    count: 0,
+  }),
+  beforeMount() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      await this.$store.dispatch('fetchAllPokemons');
+    },
+  },
+};
+</script>
+<style src="./Style.scss" lang="scss" scoped></style>
